cdc7c569584b5490be8673a20889ee210a581923
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2022-12-17 14:52:03 +0200

    correct image path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,8 +5,8 @@ function Logo() {
     <section>
       <img
         className="block w-20 h-13 mx-auto mt-8 mb-3"
-        src="/images/iaps-logo.png"
-        alt={IapsLogo}
+        src={IapsLogo}
+        alt="IAPS Logo"
       />
       <div className="block mx-5 text-textColor text-lg font-bold text-center">
         International Association of Physics Students
